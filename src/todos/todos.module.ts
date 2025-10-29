3cb431ff02e9d0f81aa10b2060883a3b4b7853ae
Juan Cruz Roldan <juancruz.roldan19@gmail.com>
2025-10-29 18:02:16 -0300

    lesson-1

---
+++ src/todos/todos.module.ts
@@ -0,0 +1,10 @@
+import {Module} from '@nestjs/common';
+import { TodosService } from './todos.service';
+import { TodosController } from './todos.controller';
+
+@Module({
+  imports: [],
+  controllers: [TodosController],
+  providers: [TodosService],
+})
+export class TodosModule {}
